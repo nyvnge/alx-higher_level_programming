c74fd4fdb9b6e73c91e4ea082db7f56dc4d19a1d
Sanderson Nyange <sandersonnyange@gmail.com>
2023-03-09 10:50:12 -0800

    a program that imports the variable a from the file variable_load_5.py and prints its value.

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+   import sys
+    sys.path.append("variable_load_5")
+    import variable_load_5 as i
+    print(i.a)
