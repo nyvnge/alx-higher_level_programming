54a305045acac7543d1500bb0059b9d2595a2b48
Sanderson Nyange <sandersonnyange@gmail.com>
2023-03-09 10:58:47 -0800

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
     import sys
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
-
+    from calculator_1 import add, sub, mul, div
+    argc = len(sys.argv)
+    for str in range(argc):
+        if (argc != 4):
+            print("Usage: {:s} <a> <operator> <b>".format(sys.argv[str]))
+            exit(1)
+        else:
+            a = int(sys.argv[1])
+            b = int(sys.argv[3])
+            if (sys.argv[2] == '+'):
+                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            elif (sys.argv[2] == '-'):
+                print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+            elif (sys.argv[2] == '*'):
+                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+            elif (sys.argv[2] == '/'):
+                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+            else:
+                print("Unknown operator. Available operators: +, -, * and /")
+                exit(1)
+            exit(0)
