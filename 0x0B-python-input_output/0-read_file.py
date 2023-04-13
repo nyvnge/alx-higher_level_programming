3065c4f88f65b918bb302a10aa8a69c0aa258ac8
Sanderson Nyange <sandersonnyange@gmail.com>
2023-04-13 10:35:46 -0700

    a function that reads a text file (UTF8) and outputs it to stdout:

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""read_file function"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and outputs it to stdout"""
+
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end=""
