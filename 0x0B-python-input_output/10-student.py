98d09576434728fe6467286b90963dd7b63647ce
Sanderson Nyange <sandersonnyange@gmail.com>
2023-04-13 11:22:52 -0700

    a class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines student class"""
+
+
+class Student:
+    """student representation"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        arguments:
+            first_name (str): first name of student.
+            last_name (str): last name of student.
+            age (int): age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
