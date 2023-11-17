31254cc3bfac465ab3451c1a068df6b898c7f602
Sanderson Nyange <101863068+nyvnge@users.noreply.github.com>
2023-11-17 14:37:22 +0300

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++
@@ -1,6 +0,0 @@
--- script that lists all shows from `hbtn_0d_tvshows_rate` by their rating
-SELECT name, SUM(rate) AS rating FROM tv_genres AS tg
-INNER JOIN tv_show_genres AS sg ON tg.id = sg.genre_id
-INNER JOIN tv_show_ratings AS tr ON sg.show_id = tr.show_id
-GROUP BY name
-ORDER BY rating DESC;
--- 0x0E-SQL_more_queries/6-states.sql
+++
@@ -1,6 +0,0 @@
--- script that creates the database hbtn_0d_usa and the table states on MySQL server
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
-);
