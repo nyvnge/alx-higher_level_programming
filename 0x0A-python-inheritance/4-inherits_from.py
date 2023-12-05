7210a6c4425b4555b8e1b2a4a8e78a1032e7e05f
nyvnge <sandersonnyange@gmail.com>
2023-12-05 08:47:00 -0800

    Few Changes

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,3 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from the specified class; otherwise False
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
-
